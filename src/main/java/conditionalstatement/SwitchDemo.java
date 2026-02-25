19b7cddc8d879a4f436dcd99087e58826a215960
adithya <sradithya97@gmail.com>
2026-02-25 14:48:02 +0530

    switch demo files are added

--- src/main/java/conditionalstatement/SwitchDemo.java
+++ src/main/java/conditionalstatement/SwitchDemo.java
@@ -1,2 +1,33 @@
-package conditionalstatement;public class SwitchDemo {
+package conditionalstatement;
+
+import java.util.Scanner;
+
+public class SwitchDemo {
+
+    public static void main(String[] args){
+        //int day=3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int day = sc.nextInt();
+        switch (day){
+            case 1:
+                System.out.println("Day is monday");
+                break;
+            case 2:
+                System.out.println("Day is Tuesday");
+                break;
+            case 3:
+                System.out.println("Day is Wednesday");break;
+            case 4:
+                System.out.println("Day is Thursday");break;
+            case 5:
+                System.out.println("Day is friday");break;
+            case 6:
+                System.out.println("Day is Sat");break;
+            case 7:
+                System.out.println("Day is sunday");break;
+            default:
+                System.out.println("invalid day");
+        }
+    }
 }
--- src/main/java/conditionalstatement/SwitchDemo2.java
+++ src/main/java/conditionalstatement/SwitchDemo2.java
@@ -1,2 +1,29 @@
-package conditionalstatement;public class SwitchDemo2 {
+package conditionalstatement;
+
+import java.util.Scanner;
+
+public class SwitchDemo2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the character: ");
+        char vowel= sc.next().charAt(0);
+
+        switch (vowel){
+            case 'a':
+                System.out.println("A is a vowel");break;
+            case 'e':
+                System.out.println("E is avowel");break;
+            case 'i':
+                System.out.println("I is a vowel");break;
+            case 'o':
+                System.out.println("O is a vowel");break;
+            case 'u':
+                System.out.println("U is avowel");break;
+
+            default:
+                System.out.println("Not a vowel");
+        }
+
+
+    }
 }
